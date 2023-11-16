10dcc821db197d4f3adb420e489ec0626e085307
kofla10 <juank_milo94@hotmail.com>
2023-11-15 22:57:44 -0500

    routes and usecontext

---
+++ src/09-contex/MainApp.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+import { Navigate, Route, Router, Routes } from 'react-router-dom' //para crear las rutas nos aseguramos de realizar el import
+import { HomePage } from './HomePage'
+import { AboutPage } from './AboutPage'
+import { LoginPage } from './LoginPage'
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>Main App</h1>
+        <hr />
+        {/* Forma de declarar las rutas */}
+        <Routes>
+          <Route path="/" element = { <HomePage/> } />
+          <Route path="login" element = { <LoginPage/> } />
+          <Route path="about" element = { <AboutPage/> } />
+
+          {/* esta forma se puede declarar una ruta por defecto en caso tal de que no se encuentre la ruta que se ingreso y usando esta forma la url no se modifica */}
+          {/* <Route path='/*' element={ <LoginPage/> } /> */}
+
+          {/* otro caso y el mas recomendado podemos usar el navigate, usando el navigate, la url se modifica con la ruta que le asignemos */}
+          <Route path='/*' element={ <Navigate to='/login' /> } />
+
+        </Routes>
+    
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import Padre from './07-tarea-memo/Padre'
+import { MainApp } from './09-contex/MainApp'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre/>
-  // </React.StrictMode>,
+  <BrowserRouter> 
+    {/* <React.StrictMode> */}
+      <MainApp/>  
+    {/*  </React.StrictMode>, */}
+  </BrowserRouter>
 )
